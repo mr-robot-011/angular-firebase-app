ae5c059dd116b714ec74a882076b24328ac9abf4
Mishra <chinmmishra@deloitte.com>
2023-02-24 19:39:06 +0530

    first commit

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -26,7 +26,7 @@ export class AuthService {
   signup(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDb0xTaRAoxyCgvaDF3kk5VYOsTwB_3o7Y',
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCyT-Gk-ODRYwvX7-NEygHf7_8rhSym75o',
         {
           email: email,
           password: password,
@@ -49,7 +49,7 @@ export class AuthService {
   login(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDb0xTaRAoxyCgvaDF3kk5VYOsTwB_3o7Y',
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCyT-Gk-ODRYwvX7-NEygHf7_8rhSym75o',
         {
           email: email,
           password: password,
